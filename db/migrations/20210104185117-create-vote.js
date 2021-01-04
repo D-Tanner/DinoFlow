80570d7b1ed85ed7a72793f9b64859e265419a05
Dillon <dft609@gmail.com>
2021-01-04 13:34:46 -0700

    added initial models and migrations and routes folder for questions

---
+++ db/migrations/20210104185117-create-vote.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Votes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      answerId: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      upvotes: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      downvotes: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Votes');
+  }
+};
