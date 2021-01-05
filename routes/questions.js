2ef153da673331e06bf51cb8b587cb30118c150e
Dillon <dft609@gmail.com>
2021-01-05 15:17:08 -0700

    I can't get question queries working

--- routes/questions.js
+++ routes/questions.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const csrf = require('csurf')
-const { Question } = require('../db/models/question')
+const { Question, Answer } = require('../db/models')
 const { check, validationResult } = require('express-validator')
 
 const csrfProtection = csrf({ cookie: true })
@@ -11,7 +11,7 @@ const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).ca
 
 /* GET users listing. */
 router.get('/ask-question', csrfProtection, asyncHandler(async (req, res, next) => {
-  res.render('ask-question-page', { title: 'Ask a Question', csrfToken: req.csrfToken() })
+  res.render('ask-question-page', { title: 'Ask a Question', question: {}, csrfToken: req.csrfToken() })
 }));
 
 const questionValidators = [
@@ -30,9 +30,10 @@ router.post('/ask-question', csrfProtection, questionValidators, asyncHandler(as
 
   const validatorErrors = validationResult(req);
 
-  if (validarotErrors.isEmpty()) {
-    //I need to assign a question ID for each question that is posted
+  if (validatorErrors.isEmpty()) {
+    //I need to assign a question ID for each question that is posted ???
     await Question.create({ title, content, userId: req.session.user.id })
+    // const question = await Question.findByPk() // ?????????????
     return res.redirect('/')
   } else {
     errors = validatorErrors.array().map((error) => error.msg)
@@ -47,7 +48,12 @@ router.post('/ask-question', csrfProtection, questionValidators, asyncHandler(as
 }));
 
 router.get('/question/:id(\\d+)', csrfProtection, asyncHandler(async (req, res, next) => {
-
+  // const question = await Question.findByPk(req.params.id, { include: { model: Answer } })
+  //const questionId = parseInt(req.params.id, 10)
+  // console.log(questionId)
+  //const question = await Question.findByPk(questionId)
+  const question = Question.findAll()
+  res.render('question', { title: 'Question', question, csrfToken: req.csrfToken() },)
 }));
 
 
