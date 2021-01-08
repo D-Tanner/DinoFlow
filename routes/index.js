40aa01202b0d474c807c6ad25d891ee8f488679c
Dillon <dft609@gmail.com>
2021-01-08 13:51:11 -0700

    added to styling to asked by function

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { Question } = require('../db/models')
+const { Question, User } = require('../db/models')
 const { asyncHandler } = require('./utils')
 /* GET home page. */
 router.get('/', asyncHandler(async (req, res, next) => {
   const questions = await Question.findAll({
     order: [
       ['createdAt', 'DESC']
-    ]
+    ],
+    include: ['Answers', { model: User, attributes: ['username'] }],
   })
   res.render('home-page', { title: 'Questions', questions });
 }));
