3a3748bce3964e885fdb63f82546932eb2247bd8
Dillon <dft609@gmail.com>
2021-01-10 17:17:25 -0700

    added correct database_url for heroku launch

--- config/database.js
+++ config/database.js
@@ -13,7 +13,7 @@ module.exports = {
     logging: false,
   },
   production: {
-    use_env_variable: 'DATABASE_URL',
+    use_env_variable: 'postgres://mzacultnilrsxv:094e5c21f95e612387ec67c56d9702ba774ab86bc1675604503615dd10976588@ec2-3-216-181-219.compute-1.amazonaws.com:5432/dbthhtgqinedtl',
     dialect: 'postgres',
     seederStorage: 'sequelize',
   }
